21f250b72d66611d1a3dcd1ce1ee5bd7636c7bfc
Sidney Alex <silvercenturion2@gmail.com>
2026-03-23 01:09:43 -0300

    feat(frontend): setup frontend

---
+++ app/App.vue
@@ -0,0 +1,3 @@
+<template>
+<h1>Hello World 123</h1>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import { nitro } from "nitro/vite";
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [nitro()],
+  plugins: [nitro(), vue(), tailwindcss()],
   resolve: {
     tsconfigPaths: true,
   },
